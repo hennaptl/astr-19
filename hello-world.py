10e6ddb6fd92389549892cfaecc8072c9a9e96cc
hennaptl <hennarpatel@gmail.com>
2025-10-16 15:18:01 -0700

    Code Journal #1

---
+++ hello-world.py
@@ -0,0 +1,24 @@
+ #This program
+ #will print hello
+ #world
+
+ #this function
+ #thells the computer to print
+ # "Hello World"
+
+def PrintHelloWorld():
+	print(f'Full Name: Henna Patel')
+	print(f'Pronouns: She/Her')
+	print(f'Favorite Movie: Sinners, it was so exciting and detailed.')
+	print(f'Favorite Food: Dosa, specifically made by my aunt.')
+
+
+ 	#This defines our main()
+ 	#fuctions fro our program
+def main():
+ 		PrintHelloWorld()
+
+ #When we run this program
+ #this executes first
+if __name__=="__main__":
+		main()
